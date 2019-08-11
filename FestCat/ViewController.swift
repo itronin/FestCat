66fbcdd552c0050fc50937a76efa2e7bc8333684
Igor Tronin <tronin.88@gmail.com>
2019-08-11 22:36:45 +0300

    Initial Commit

---
+++ FestCat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FestCat
+//
+//  Created by Igor Tro on 11/08/2019.
+//  Copyright © 2019 Igor Tro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
